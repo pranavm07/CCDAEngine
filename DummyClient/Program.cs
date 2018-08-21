5ef7b527fec737be8351b38f18c02ad24d61ab5e
pranav <pranav@nalashaa.com>
2018-08-21 16:25:54 +0530

    added app config and my sql connection code into dummy client

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Xml;
 using ICCDA;
 using Schematron;
+using MySql.Data.MySqlClient;
 
 namespace DummyClient
 {
@@ -16,6 +17,38 @@ namespace DummyClient
         {
             try
             {
+                MySqlConnection mysqlconnection;
+                string server;
+                string database;
+                string uid;
+                string password;
+
+                server = "localhost";
+                database = "testdb";
+                uid = "root";
+                password = "123456";
+                string connectionString;
+                connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+                List<string> list = new List<string>();
+                mysqlconnection = new MySqlConnection(connectionString);
+
+                mysqlconnection.Open();
+                string query = "select * from user";
+                MySqlCommand cmd = new MySqlCommand(query, mysqlconnection);
+
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    list.Add(dataReader["id"] + "");
+                    list.Add(dataReader["firstname"] + "");
+                    list.Add(dataReader["lastname"] + "");
+                }
+                dataReader.Close();
+
+                mysqlconnection.Close();
+
                 Validator validator = new Validator();
                 validator.AddSchema("E:/CDA_SDTCSchema/infrastructure/cda/CDA_SDTC.xsd");
                 validator.Validate("E:/CCDASchema/sample/validCCDA.xml");
@@ -27,6 +60,19 @@ namespace DummyClient
                 //schematron.AddSchema(schematronSchema);
                 //schematron.Validate(instance);
 
+            }
+            catch (MySqlException myex)
+            {
+                switch (myex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
             }
             catch (Exception ex)
             {
